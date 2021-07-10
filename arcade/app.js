afd6281c2d4dfaddca0aec098b059d016b1b1751
Shane D. Tyler <soniccinema@yahoo.com>
2021-07-10 13:18:37 -0700

    added button and title

--- arcade/app.js
+++ arcade/app.js
@@ -4,7 +4,14 @@ let testElem = document.querySelector('#board');
 console.log('testElem: ', testElem);
 
 // ***************** STATE *****************
-let state = {};
+const gameState = {
+    players: ['X', 'O'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+};
 
 function resetState() {
     state.board = ['O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
@@ -17,8 +24,8 @@ let boardElem = document.querySelector('#board');
 function renderBoard() {
     // empty the element
     boardElem.innerText = '';
-    for (let i = 0; i < state.board.length; ++i) {
-        let card = state.board[i];
+    for (let i = 0; i < gameState.board.length; ++i) {
+        let card = gameState.board[i];
         // create a cell
         let cellElem = document.createElement('div');
         cellElem.classList.add('cell');
