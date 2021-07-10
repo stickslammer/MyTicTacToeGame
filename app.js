287db72b39fdaf7aaac4caeea036107c3c5b1daf
stickslammer <soniccinema@gmail.com>
2021-07-10 11:32:43 -0700

    First uploads after creating files via touch
    Files were created using 'touch' and now have text within, allowing upload.

---
+++ app.js
@@ -0,0 +1,43 @@
+// initial setup
+// testing
+let testElem = document.querySelector('#board');
+console.log('testElem: ', testElem);
+
+// ***************** STATE *****************
+let state = {};
+
+function resetState() {
+    state.board = ['car', 'bus', 'car', 'cat', 'dog', 'cat', 'dog', 'hat', 'hat', 'bus', 'mit', 'mit'];
+}
+
+// ***************** DOM SELECTORS *****************
+let boardElem = document.querySelector('#board');
+
+// ***************** DOM MANIPULATION FUNCTIONS *****************
+function renderBoard() {
+    // empty the element
+    boardElem.innerText = '';
+    for (let i = 0; i < state.board.length; ++i) {
+        let card = state.board[i];
+        // create a cell
+        let cellElem = document.createElement('div');
+        cellElem.classList.add('cell');
+        cellElem.dataset.index = i;
+        cellElem.innerHTML = card;
+        boardElem.appendChild(cellElem);
+    }
+}
+
+// ***************** EVENT LISTENERS *****************
+boardElem.addEventListener('click', function (event) {
+    if (event.target.className === 'cell') {
+        console.log('event.target: ', event.target);
+        let cellIdx = event.target.dataset.index;
+        console.log('cellIdx: ', cellIdx);
+        renderBoard();
+    }
+})
+
+// ***************** BOOTSTRAPPING *****************
+resetState();
+renderBoard();
