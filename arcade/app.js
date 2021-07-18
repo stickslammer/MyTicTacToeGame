41c93288437a498268f883a424972854ac878691
Shane D. Tyler <soniccinema@yahoo.com>
2021-07-17 21:25:18 -0700

    made positive changes to computerMove

--- arcade/app.js
+++ arcade/app.js
@@ -57,7 +57,7 @@ function checkForPlayers(nameStorage) {
   `;
         }
 
-        getNames();
+    getNames();
         
         // Setting DOM to all boxes displayed
         var b1, b1, b3, b4, b5, b6, b7, b8, b9;
